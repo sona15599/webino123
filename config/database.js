a50644ed87177effe3c9102dc137865ee1faadbd
sona15599 <you@example.com>
2019-03-12 19:48:18 +0530

    Second commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb+srv://phenom:1708@technotest-a8hf3.mongodb.net/test?retryWrites=true',
+  //database: 'mongodb+srv://phenom:1708@technotest-a8hf3.mongodb.net/test?retryWrites=true',
+database: 'sona15599:156111@sonam@cluster0-rsm4y.mongodb.net/test?retryWrites=true',
     secret: 'phenom'
 }
